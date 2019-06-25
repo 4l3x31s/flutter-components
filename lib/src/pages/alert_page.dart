5ef1445f08e1178b467cd57e8e04767bbe8dc942
Alexeis Vladimir Carrillo Pinaya <acarrillo@mail.rua.net>
2019-06-25 13:26:26 -0400

    Pagina de inputs

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -13,9 +13,7 @@ class AlertPage extends StatelessWidget {
         child: RaisedButton(
           child: Text('Mostrar Alerta'),
           color: Colors.blue,
-          onPressed: () {
-
-          },
+          onPressed: () => _mostrarAlert(context),
           textColor: Colors.white,
           shape: StadiumBorder(),
         ),
@@ -26,6 +24,42 @@ class AlertPage extends StatelessWidget {
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  void _mostrarAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0)
+          ),
+          title: Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Este es el contenido de la caja de la alerta'),
+              FlutterLogo(
+                size: 100.0,
+              )
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancelar'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      }
     );
   }
 }
